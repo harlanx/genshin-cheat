d85ec909514f1b5ffdcdaf99c50772c4d6517e82
HarrySilan <silan.harry@gmail.com>
2022-05-02 00:58:57 +0800

    faster & uniform randomization

--- cheat-library/src/user/cheat/player/RapidFire.cpp
+++ cheat-library/src/user/cheat/player/RapidFire.cpp
@@ -145,7 +145,10 @@ namespace cheat::feature
 		}
 		if (f_Randomize)
 		{
-			countOfAttacks = rand() % (f_maxMultiplier.value() - f_minMultiplier.value()) + f_minMultiplier.value();
+			std::random_device rd;
+			std::mt19937 rng(rd());
+			std::uniform_int_distribution<std::mt19937::result_type> dist(f_maxMultiplier.value(), f_minMultiplier.value());
+			countOfAttacks = dist(rng);
 			return countOfAttacks;
 		}
 
